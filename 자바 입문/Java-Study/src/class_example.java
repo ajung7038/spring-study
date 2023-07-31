801ca24171ad91fca9d686af546cc51d895c2102
ajeong <ajung7038@naver.com>
2023-07-31 14:05:23 +0900

    클래스 예제 수정

--- 자바 입문/Java-Study/src/class_example.java
+++ 자바 입문/Java-Study/src/class_example.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class class_example {
+public class class_example
+{
+    public class Student {
+        int studentID;
+        String studentName;
+        int grade;
+        String address;
+
+        public void showStudentInfo(){
+            System.out.println(studentName + "," + address);
+        }
+    }
+    public static void main(String[] args)
+    {
+        int num1 = 10;
+        int num2 = 20;
+        
+        int sum = add(num1, num2);
+        System.out.println(num1 + " + " + num2 + " = " + sum + " 입니다.");
+    }
+    
+    public static int add(int n1, int n2) {
+        int result = n1 + n2;
+        return result; //결과값 반환
+    }
 }

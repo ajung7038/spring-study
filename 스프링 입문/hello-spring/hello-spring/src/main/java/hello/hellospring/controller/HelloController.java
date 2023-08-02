4c00f9cfe6a76de7043fad3bbda5eb27004de223
ajeong <ajung7038@naver.com>
2023-08-02 19:08:13 +0900

    스프링 입문 S1 : 프로젝트 환경설정

--- 스프링 입문/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ 스프링 입문/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
+
+        // Data에 "hello!!"를 넣어놨기 때문에 /hello로 접속했을 시 `안녕하세요. hello!!` 가 뜨게 된다.
         model.addAttribute("data", "hello!!");
+
+        // hello.html 파일을 찾아 렌더링
         return "hello";
     }
 }

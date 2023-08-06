93aedd051dc106a58198b7ffe66698c2110190a7
ajeong <ajung7038@naver.com>
2023-08-06 13:38:13 +0900

    S6 : 스프링 데이터 JPA

--- 스프링입문/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 스프링입문/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -11,29 +11,38 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-   /* private final DataSource dataSource;
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+/* private final DataSource dataSource;
 
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }*/
 
-    private EntityManager em;
+    /*private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
-    }
+    }*/
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
+}
 
-    @Bean
+/*    @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+//        return new JpaMemberRepository(em);
+
     }
-}
+}*/

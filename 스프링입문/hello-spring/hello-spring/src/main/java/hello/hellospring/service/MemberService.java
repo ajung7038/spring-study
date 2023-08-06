146bc85333e17ffbf58ad9d452d2ad197f500ea0
ajeong <ajung7038@naver.com>
2023-08-06 14:29:38 +0900

    AOP 로직 정리

--- 스프링입문/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ 스프링입문/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -26,7 +26,12 @@ public class MemberService {
     // 회원 가입
     public Long join(Member member){
         // 같은 이름이 있는 중복 회원 X
-        long start = System.currentTimeMillis();
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+
+
+        /*long start = System.currentTimeMillis();
 
         try {
             // 중복 회원 검증
@@ -38,7 +43,7 @@ public class MemberService {
             long finish = System.currentTimeMillis();
             long timeMs = finish - start;
             System.out.println("join = " + timeMs + "ms");
-        }
+        }*/
     }
 
     private void validateDuplicateMember(Member member) {

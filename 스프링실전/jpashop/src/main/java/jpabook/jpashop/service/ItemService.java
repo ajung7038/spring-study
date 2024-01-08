8db27aac54e213a93abdc11ae7989bc939160abf
ajeong <ajung7038@naver.com>
2024-01-08 23:15:15 +0900

    Feat: 스프링 실전 - 변경 감지와 병합

--- 스프링실전/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ 스프링실전/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,14 @@ public class ItemService {
     @Transactional // readOnly니까 오버라이딩 (우선권 가짐)
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); // id를 기반으로 실제 db에 있는 영속성 상태의 엔티티 찾아오기
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
